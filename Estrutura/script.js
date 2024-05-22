29b43decf80e3292d5b717597137c7ce5e69eb7b
Italo Git <sulamita.italo12@gmail.com>
2024-05-22 14:39:12 -0300

    add

---
+++ Estrutura/script.js
@@ -0,0 +1,9 @@
+function pagar() {
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "PAGAMENTO REALIZADO COM SUCESSO",
+        showConfirmButton: false,
+        timer: 2500
+      });
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+O StudentCard é um projeto acadêmico que tem como objetivo principal ter um sistema que o aluno universitário consiga parcelar o valor do boleto da sua mensalidade.
