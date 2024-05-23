3b12bd4a2ce11f9579aa68fca5d1a1296f4fb46b
Jhonatha Camargo <jhonemmaje@gmail.com>
2024-05-22 22:21:14 -0300

    clear

--- Estrutura/script.js
+++
@@ -1,9 +0,0 @@
-function pagar() {
-    Swal.fire({
-        position: "center",
-        icon: "success",
-        title: "PAGAMENTO REALIZADO COM SUCESSO",
-        showConfirmButton: false,
-        timer: 2500
-      });
-}
